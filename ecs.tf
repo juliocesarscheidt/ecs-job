78de54d6779dc8f56620d5c9310c47f87c62add7
juliocesarscheidt <julio.scheidt96@gmail.com>
2021-05-12 00:29:11 -0300

    Update ecs cluster

--- ecs.tf
+++ ecs.tf
@@ -1,5 +1,12 @@
 resource "aws_ecs_cluster" "ecs-cluster" {
   name = var.ecs_cluster_name
+
+  capacity_providers = ["FARGATE_SPOT", "FARGATE"]
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+  }
+
   tags = merge(var.tags, {
     "Name" = var.ecs_cluster_name
   })
